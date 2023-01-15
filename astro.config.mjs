69d787f890a8f1a82eb4440d01c26a3d6bc076ed
Vikas Ganiga <codewithvikas@gmail.com>
2023-01-15 10:05:52 +0000

    feat: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,12 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-cip.netlify.app/",
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), react()]
 });
---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>;
+};
+
+export default Counter;
