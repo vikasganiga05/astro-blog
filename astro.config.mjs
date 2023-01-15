774fccb8940d9e12a8ac2c535de1d9a4b6cb9564
Vikas Ganiga <codewithvikas@gmail.com>
2023-01-15 10:46:47 +0000

    feat: add partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-cip.netlify.app/",
-  integrations: [image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), react(), tailwind()]
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    react(),
+    tailwind(),
+    partytown(),
+  ],
 });
