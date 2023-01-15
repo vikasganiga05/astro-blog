da694dc77319ac5f3090cbedc37ef3c9a6820a48
Vikas Ganiga <codewithvikas@gmail.com>
2023-01-15 10:34:12 +0000

    feat: add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,12 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-cip.netlify.app/",
   integrations: [image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), react()]
+  }), sitemap(), react(), tailwind()]
 });
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>;
+  return (
+    <button
+      className="bg-blue-400 text-blue-900"
+      onClick={() => setCount((prev) => prev + 1)}
+    >
+      {count}
+    </button>
+  );
 };
 
 export default Counter;
